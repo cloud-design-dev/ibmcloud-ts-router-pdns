39ca5674f205d7b329a17c0fc419d5b66bf193fa
greyhoundforty <ryantiffany@fastmail.com>
2024-09-27 14:28:18 -0500

    Catch up commit

--- modules/testing/output.tf
+++
@@ -1,3 +0,0 @@
-output "demo_floating_ip" {
-  value = ibm_is_floating_ip.demo.address
-}
--- modules/testing/variables.tf
+++
@@ -1,5 +0,0 @@
-variable "prefix" {}
-variable "zone" {}
-variable "resource_group_id" {}
-variable "vnic_id" {}
-variable "tags" {}
